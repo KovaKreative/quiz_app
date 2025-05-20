7c0a4c34fa4aca64ebfe3852186df4eddc9c9727
KovaKreative <eric.kovalevskyy@gmail.com>
2025-05-20 17:46:25 -0400

    Refactor MyApp and tweak style

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(backgroundColor: const Color.fromARGB(255, 40, 32, 128), body: Quiz()),
-    ),
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  build(context) {
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: const Color.fromARGB(255, 40, 32, 128),
+          body: Quiz()),
+    );
+  }
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -12,25 +12,23 @@ class StartScreen extends StatelessWidget {
   Widget build(context) {
     return Center(
       child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/images/quiz-logo.png',
-                width: 300, color: const Color(0xDDFFD900)),
-            spacer,
-            Text(
-              'Learn Flutter the fun way!',
-              style: GoogleFonts.oxanium(color: Color(0xFFFFD900), fontSize: 24)
-            ),
-            spacer,
-            OutlinedButton.icon(
-                onPressed: buttonFunction,
-                style:
-                    OutlinedButton.styleFrom(foregroundColor: Color(0xDDFFD900)),
-                icon: const Icon(Icons.arrow_right),
-                label: const Text("Start Quiz"))
-          ],
-        ),
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/quiz-logo.png',
+              width: 300, color: const Color(0xDDFFD900)),
+          spacer,
+          Text('Learn Flutter the fun way!',
+              style:
+                  GoogleFonts.oxanium(color: Color(0xFFFFD900), fontSize: 24)),
+          spacer,
+          OutlinedButton.icon(
+              onPressed: buttonFunction,
+              style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+              icon: const Icon(Icons.arrow_right),
+              label: const Text("Start Quiz"))
+        ],
+      ),
     );
   }
 }
